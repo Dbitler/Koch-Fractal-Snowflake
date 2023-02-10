71cf8ba68f9e6073d3ef5a48242e59a55de24c73
Dbitler <dbitler@hawk.iit.edu>
2023-02-10 17:27:08 -0600

    Initial Commit

---
+++ Koch-Fractal-Snowflake/Koch_Fractal_SnowflakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Koch_Fractal_SnowflakeApp.swift
+//  Koch-Fractal-Snowflake
+//
+//  Created by IIT PHYS 440 on 2/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Koch_Fractal_SnowflakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
